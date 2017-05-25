3d385f0a69c2ef164d63b0f661b89bcb00eb8039
yuji kawashima <yuji.kawashima@aktsk.jp>
2017-05-25 10:37:12 +0900

    fix HumanInterfaceName, and Add HumanClass

--- human.cs
+++ human.cs
@@ -1,9 +1,16 @@
 using System;
 
-interface Human{
+interface IHuman{
   void Walk(float speed);
   void Speak(string text);
   string listen();
   void Run(float speed);
   void Get(object ob);
+}
+
+class Human{
+  int age = 0;
+  float weight = 0;
+  float height = 0;
+  string DefaultLanguage = "";
 }
